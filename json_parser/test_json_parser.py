e985355712a246fb6d98ab96f92e0e53bc5877b6
root <khalidfaraj8@gmail.com>
2024-06-30 18:29:39 +0800

    kuberentes practice and learning

--- json_parser/test_json_parser.py
+++ json_parser/test_json_parser.py
@@ -4,6 +4,7 @@ class TestJsonLexerFromFile(unittest.TestCase):
     def __init__(self,text):
         self.text = text
 
-
+    def test_step1(self):
+        pass
 if __name__ == "__main__":
     unittest.main()
